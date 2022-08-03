50095a5fdc8faeee31a277595e97bb0dea725aff
lesmemesreborn <92157744+lesmemesreborn@users.noreply.github.com>
2022-08-03 10:21:11 +0300

    added scripts

---
+++ scripts/sidebar-menu.js
@@ -0,0 +1,7 @@
+const openingBtn = document.querySelector(".sidebar__hamburger");
+const closingBtn = document.querySelector(".sidebar__close");
+const sidebar = document.querySelector(".sidebar");
+
+openingBtn.addEventListener("click", function () {
+  sidebar.classList.add("sidebar--opened");
+});
